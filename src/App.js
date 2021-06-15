99d1f721625cacb8685a7945c1e371f6da1528a5
Vignesh <vigneshm1797@gmail.com>
2021-06-15 21:35:09 +0530

    added imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import Card from './components/Cards/Card';
+import SwipeButton from './components/SwipeButtons/SwipeButton';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Card />
+      <SwipeButton />
     </div>
   );
 }
