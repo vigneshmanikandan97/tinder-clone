f0307cc1b805b786478dacc3b08fbca0cf36f4b7
Vignesh <vigneshm1797@gmail.com>
2021-06-26 00:42:32 +0530

    added routes to main and chat paths

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import Header from './components/Header/Header';
 import Card from './components/Cards/Card';
 import SwipeButton from './components/SwipeButtons/SwipeButton';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Card />
-      <SwipeButton />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/chat">
+            <h1>You've reached the chats page.</h1>
+          </Route>
+          <Route path="/">
+            <Card />
+            <SwipeButton />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // STYLESHEET IMPORTS
 import './Header.css';
@@ -14,15 +15,19 @@ import TinderLogo from "./../../assets/images/tinder-logo.svg";
 function Header() {
     return (
         <div className="header">
-            <IconButton>
-                <PersonIcon fontSize="large" className="header__icon"/>
-            </IconButton>
+            <Link to="/">
+                <IconButton>
+                    <PersonIcon fontSize="large" className="header__icon"/>
+                </IconButton>
+            </Link>
 
             <img className="header__logo" src={TinderLogo} alt="tinder-logo"/>
 
-            <IconButton>
-                <ChatIcon fontSize="large" className="header__chat" />
+            <Link to="/chat">
+                <IconButton>
+                    <ChatIcon fontSize="large" className="header__chat" />
                 </IconButton>
+            </Link>
         </div>
     )
 }
