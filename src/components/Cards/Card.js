4ac662ce533ca84909ad5e7a8f40b77f6ce34f04
Vignesh <vigneshm1797@gmail.com>
2021-07-07 12:58:52 +0530

    updated tinder card container

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -6,6 +6,20 @@ import database from './../../firebase';
 function Card() {
 
     const [people, setPeople] = useState([]);
+    let [count, setCount] = useState(0);
+
+    window.setTimeout(() => {
+        const tinderCards = document.querySelectorAll(".swipe");
+        const tinderCardsContainer = document.querySelector(".tinderCards__cardContainer");
+        
+        setCount(() => {
+            count = tinderCards.length;
+        });
+
+        if (!tinderCardsContainer.classList.contains("addBoxShadow")) {
+            tinderCardsContainer.classList.add("addBoxShadow");
+        }
+    }, 500);
 
     useEffect(() => {
         // Using Cloud Firestore
@@ -29,16 +43,31 @@ function Card() {
     }, []);
 
     const swiped = (direction, nameToDelete) => {
+        const tinderCardsContainer = document.querySelector(".tinderCards__cardContainer");
+        if (count === 1) {
+            if (tinderCardsContainer.classList.contains("addBoxShadow")) {
+                tinderCardsContainer.classList.remove('addBoxShadow');
+                tinderCardsContainer.classList.add('removeBoxShadow');
+            }
+        } else {
+            if (tinderCardsContainer.classList.contains("removeBoxShadow")) {
+                tinderCardsContainer.classList.remove('removeBoxShadow');
+                tinderCardsContainer.classList.add('addBoxShadow');
+            }
+        }
         console.debug("swiped " + direction + " on " + nameToDelete);
     };
 
     const outOfFrame = (name) => {
+        setCount(() => {
+            count -= 1;
+        });
         console.debug("removed " + name);
     };
 
     return (
         <div className="tinderCards">
-            <div className="tinderCards__cardContainer">
+            <div className={"tinderCards__cardContainer"}>
                 {people.map((person) => {
                     return <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']} onSwipe={(dir) => swiped(dir, person.name)} onCardLeftScreen={() => outOfFrame(person.name)}>
                                 <div style={{backgroundImage: `url(${person.imageUrl})`}} className="card">
