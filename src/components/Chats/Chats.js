5d85fb3196e3d5e7d533a575287a686e3ab356b6
Vignesh <vigneshm1797@gmail.com>
2021-07-07 11:13:07 +0530

    Chat component files

---
+++ src/components/Chats/Chats.js
@@ -0,0 +1,13 @@
+import './Chats.css';
+
+import React from 'react'
+
+function Chats() {
+    return (
+        <div className="chats">
+            
+        </div>
+    )
+}
+
+export default Chats;
