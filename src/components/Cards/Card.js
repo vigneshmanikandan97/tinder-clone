4c2e56270c204b5fd3a08a6d01b1faa5d48f0dbe
Vignesh <vigneshm1797@gmail.com>
2021-07-07 11:20:31 +0530

    reduced spacing

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -39,17 +39,11 @@ function Card() {
         <div className="tinderCards">
             <div className="tinderCards__cardContainer">
                 {people.map((person) => {
-                    return <TinderCard
-                                className="swipe"
-                                key={person.name}
-                                preventSwipe={['up', 'down']}
-                                onSwipe={(dir) => swiped(dir, person.name)}
-                                onCardLeftScreen={() => outOfFrame(person.name)}
-                                >
-                                    <div style={{backgroundImage: `url(${person.imageUrl})`}} className="card">
-                                        <h3>{person.name},&nbsp;{person.age}</h3>
-                                    </div>
-                                </TinderCard>
+                    return <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']} onSwipe={(dir) => swiped(dir, person.name)} onCardLeftScreen={() => outOfFrame(person.name)}>
+                                <div style={{backgroundImage: `url(${person.imageUrl})`}} className="card">
+                                    <h3>{person.name},&nbsp;{person.age}</h3>
+                                </div>
+                            </TinderCard>
                 })}
             </div>
         </div>
